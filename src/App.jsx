cbc69dfd466f65013880d0edcc4c62e7e4c74dd7
katakatataan <yuki.matsuyama0123@gmail.com>
2022-01-28 22:47:48 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Medama} from './components.macaron'
+// import { Sample } from './Sample'
+
+function App() {
+
+  return (
+    <div>
+      {/* <Sample/> */}
+      <Medama/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Sample.jsx
@@ -0,0 +1,2 @@
+
+export const Sample = () => <div>hello</div>
