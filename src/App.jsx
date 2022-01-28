e17f7c46ee90d11cda5ab715e7775bb7da31668a
katakatataan <yuki.matsuyama0123@gmail.com>
2022-01-28 23:27:19 +0900

    update figma

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {Medama} from './components.macaron'
-// import { Sample } from './Sample'
+import { Sample } from './Sample'
 
 function App() {
 
